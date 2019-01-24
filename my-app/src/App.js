1f742bbb1b09fa2a62a3123585d73d27aee7ed7a
Shubham Singh <shubhamit3038@gmail.com>
2019-01-24 19:55:29 +0530

    Adding and use radium (:hover css sudo event)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
+
 import './App.css';
 import Person from "./Person/Person.js";
 
@@ -55,7 +57,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let person = null;
@@ -75,6 +81,10 @@ class App extends Component {
         </div>
         );
         style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
     }
 
     let classes = [];
@@ -97,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
