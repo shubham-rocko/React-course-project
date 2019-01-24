88a315a8f214278542c9f59cfe6d948a9eed17a4
Shubham Singh <shubhamit3038@gmail.com>
2019-01-24 15:07:18 +0530

    change person name dynamically onChange event bind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,14 +23,18 @@ class App extends Component {
     });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: "Shubham", age: 22},
-        {name: event.target.value, age: 33},
-        {name: "Priya", age: 28}
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons =  [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   toggleNameHandler = () => {
@@ -64,6 +68,7 @@ class App extends Component {
              age={person.age}
              key={person.id}
              clicked={() => this.deletePersonHandler(index)}
+             changed = {(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
         </div>
