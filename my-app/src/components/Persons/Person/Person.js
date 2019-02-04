21a90f7a038a2e3464c2c4a365430153c60f3d63
Shubham Singh <shubhamit3038@gmail.com>
2019-02-04 17:51:42 +0530

    Returing adjacent element

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -32,6 +32,12 @@ class Person extends Component{
           <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div>
       );
+
+      // return [
+      //   <p key="1" onClick={this.props.clicked}>Hi I'm {this.props.name} and I'm {this.props.age} years old.</p>,
+      //   <p key="2">{this.props.children}</p>,
+      //   <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
+      // ]
   }
 }
 
