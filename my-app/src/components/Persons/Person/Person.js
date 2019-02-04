cef9b469e18c0963d64d2215e60013ef0bf009f7
Shubham Singh <shubhamit3038@gmail.com>
2019-02-04 14:03:31 +0530

    lifeCycle of stateFull component

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -3,7 +3,26 @@ import React, {Component} from "react";
 import classes from "./Person.css";
 
 class Person extends Component{
+
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] Inside Constructor', this.props);  
+  }
+
+  componentWillMount(){
+    console.log('[Person.js] Inside componentWillMount()');
+  }
+  
+  componentDidMount(){
+    console.log('[Person.js] Inside componentDidMount()');
+  }
+
+  componentWillUnmount(){
+    console.log('[Person.js] Inside componentWillUnmount()');
+  }
+
   render() {    
+    console.log('[Person.js] Inside render()')
       return (
         <div className={classes.Person}>
           <p onClick={this.props.clicked}>
--- my-app/src/components/Persons/Persons.js
+++ my-app/src/components/Persons/Persons.js
@@ -3,7 +3,25 @@ import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[Persons.js] Inside Constructor', this.props);  
+  }
+
+  componentWillMount(){
+    console.log('[Persons.js] Inside componentWillMount()');
+  }
+  
+  componentDidMount(){
+    console.log('[Persons.js] Inside componentDidMount()');
+  }
+
+  componentWillUnmount(){
+    console.log('[Persons.js] Inside componentWillUnmount()');
+  }
+
   render() {
+    console.log('[Persons.js] Inside render()');
     return this.props.persons.map((person, index) => {
       return (
         <Person
